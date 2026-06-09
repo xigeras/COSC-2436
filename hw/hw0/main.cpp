756ae478a3b190f1efa3108de25eb6f529d7c3af
xigeras <jbharris62@gmail.com>
2026-06-09 05:10:14 -0500

    mmm

--- hw/hw0/main.cpp
+++ hw/hw0/main.cpp
@@ -1,9 +1,28 @@
+#include "ArgumentManager.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+int main(int argc, char* argv[]) {
+    ArgumentManager am(argc, argv);
+    const string input = am.get("input");
+    const string output = am.get("output");
 
-int main() {
+    ifstream inputfile(input);
+    ofstream outputfile(output);
+
+    vector<int> R;
+    vector<int> C;
+
+    for (int i = 0; i < R; i++) {
+        
+    }
+    
 
     return 0;
 }
+
+
+// research: std::vector, loop that runs x times, and inside that another loop that runs y times

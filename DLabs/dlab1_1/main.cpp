77cb546525b1fcb0f6be92e7df5d17376c63e870
xigeras <jbharris62@gmail.com>
2026-06-09 16:40:37 -0500

    dlab 1

---
+++ DLabs/dlab1_1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int n; 
+    int count = 0;
+    cin >> n;
+ 
+    while (n > 0) {
+        int max = 0;
+        int temp = n;
+
+        while (int d = temp % 10 != 0) {
+            d = max;
+        }
+        
+
+
+    }
+
+
+    cout << count << endl;
+}
+
+
+// NEED A MAX. while loop would be better

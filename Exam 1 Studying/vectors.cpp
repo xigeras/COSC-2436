c7efa2166565636ec9c1eb9890fbd2159e4230bb
xigeras <jbharris62@gmail.com>
2026-06-14 11:20:18 -0500

    jjj

---
+++ Exam 1 Studying/vectors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string> team = {"LeBron", "Michael", "Kobe"};
+
+    string team[] = {"LeBron", "Michael", "Kobe"};
+
+    return 0;
+}
